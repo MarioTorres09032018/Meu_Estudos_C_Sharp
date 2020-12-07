159eafc685f62b07af598c9cc62d6d6c65e7f5e2
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-12-06 22:37:31 -0300

    MEUS ESTUDOS 2020

---
+++ Console_CLASSES_SEALED/Console_CLASSES_SEALED/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Console_CLASSES_SEALED
+
+//UM CONCEITO SIMPLES É QUE UMA CLASSE "Sealed" NÃO PODE SER HERDADA.
+
+{
+    sealed class veiculo 
+    {
+
+    }
+
+    class Carro:veiculo  //A MENSAGEM DE ERRO JÁ INFORMA QUE A CLASSE "sealed class veiculo" NÃO PODE SER HERDADA. 
+    {
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
