6df03b16c9f07fee908f36489d1050a512fb3dba
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-12-06 22:53:32 -0300

    MEUS ESTUDOS C# 2020

---
+++ Console_MEMBROS_PROTECTED/Console_MEMBROS_PROTECTED/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Console_MEMBROS_PROTECTED
+
+    //A DIFERENÇA DO PRIVATE PARA O PROTECTED É QUE, O PRIVATE RESTRINGE O ACESSO AO MEMBRO SOMENTE A CLASSE. SÓ É POSSÍVEL ACESSAR O MEMBRO COM METODOS DA PRÓPRIA
+    //CLASSE, A CLASSE BASE. UMA CLASSE DERIVADA DE VEICULO, NÃO TERÁ ACESSO AOS MEMBROS "private" DA CLASSE BASE "Veiculo". OS MEMBROS "private" SÓ PODERÃO SER 
+    //ACESSADOS ATRAVÉS DE METODOS "public". O "protected" PERMITE O ACESSO AOS MEMBROS PELAS CLASSES DERIVADAS, TANTO DA CLASSE BASE, QUANTO DE SUAS CLASSES DERIVADAS.
+    //MAS, SOMENTE PELAS CLASSES. SE TENTARMOS ACESSAR O MEMBRO "protected" FORA DA CLASSE, PELO OBJETO, NÃO TEREMOS ACESSO.
+
+{
+    class Veiculo //NOVA CLASSE BASE CRIADA
+    {
+        public int VelociadeAtual;      //PROPRIEDADE(MEMBRO) DE NIVEL DE SEGURANÇA "public" DO TIPO "int" CRIADA PARA GUARDAR A VELOCIDADE ATUAL DO "Veiculo"
+        private int VelocidadeMaxima;    //PROPRIEDADE(MEMBRO) DE NIVEL DE SEGURANÇA "private" DO TIPO "int" CRIADA PARA GUARDAR A VELOCIDADE MAXIMA DO "Veiculo"
+        protected bool ligado;          //PROPRIEDADE(MEMBRO) DE NIVEL DE SEGURANÇA "protected" DO TIPO "bool" CRIADA PARA DETERMINAR SE "Veiculo" ESTÁ LIGADO  
+
+        public Veiculo(int VelocidadeMaxima)//METODO CONSTRUTOR "Veiculo" CRIADO DENTRO DA CLASSE BASE "Veiculo" PARA INICIAR A PROPRIEDADE "VelocidadeMaxima" 
+                                            //QUE ENTRA COMO PARÂMETRO DENTRO DO CONSTRUTOR "Veiculo".
+        {
+            VelociadeAtual = 0;
+            this.VelocidadeMaxima = VelocidadeMaxima; //OPERADOR "this" USADO PARA CHAMAR A PROPRIEDADE "VelocidadeMaxima" DA CLASSE "Veiculo" QUE É "private" INICIADO
+            ligado = false;                           //COMO PARAMETRO DENTRO DO CONSTRUTOR "Veiculo".
+        }
+
+        public bool getLigado() //METODO CRIADO PARA OBTER O "ligado" QUE É "protected".
+        {
+            return ligado;
+        }
+        public int getVelocidadeMaxima() //METODO CRIADO PARA OBTER A "Velocidade Maxima" QUE É "private".
+        {
+            return VelocidadeMaxima;
+        }
+
+        class Carro:Veiculo //CLASSE DERIVADA "Carro" QUE HERDARÁ MEMBROS DA CLASSE BASE "Veiculo".
+        {
+            public string nome; //PROPRIEDADE(MEMBRO) DE NIVEL DE SEGURANÇA "public" DO TIPO "string" DE NOME "nome" CRIADA PARA HERDAR MEMBROS DA CLASSE "Veiculo"
+            public Carro(string nome, int VelMax):base(VelMax) //METODO CONTRUTOR "Carro" DA CLASSE DERIVADA "carro".
+               
+            {
+                this.nome = nome;
+                ligado = true;
+            }
+        }
+        class Program //CLASSE PADRÃO JÁ EXISTENTE
+        {
+            static void Main()  //METODO PADRÃO JÁ EXISTENTE
+            {
+                Carro carro = new Carro("Omega", 120); //ISTANCIAÇÃO DE "Carro" COM PARAMETROS RELACIONADOS AO CONTRUTOR "public Carro(string nome, int VelMax)
+                                                       //:base(VelMax)" DA CLASSE DERIVADA "Carro" QUE HERDA AS PROPRIEDADE DE "Veiculo".
+                Console.WriteLine("Nome.............:{0}", carro.nome);
+                Console.WriteLine("Velocidade Maxima:{0}", carro.getVelocidadeMaxima());
+                Console.WriteLine("Ligado...........:{0}", carro.getLigado()); //LEMBRANDO QUE "ligado" QUE É "protected" SÓ PODE SER ACESSADO DENTRO DA CLASSE DERIVADA OU, 
+                                                                               //DENTRO DA PRÓPRIA CLASSE. POR ISSO UTILIZAMOOS O METODO "get" PARA OBTER ESTA PROPRIEDADE.
+            }
+        }
+    }
+    
+}
---
+++ Console_METODOS_VIRTUAIS/Console_METODOS_VIRTUAIS/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Console_METODOS_VIRTUAIS
+{
+    //METODOS VIRTUAIS SÃO METODOS QUE TEM O MESMO NOME, EM CLASSES DIFERENTES. UMA CLASSE QUE HERDA DA OUTRA E QUE TEM UM METODO QUE TEM EXATAMENTE O MESMO NOME DA SUA
+    //CLASSE BASE. PORÉM ESTE METODO IRÁ EXECUTAR UMA AÇÃO DIFERENTE. E DEVEMOS DEFINIR NA CLASSE BASE QUE ESTE METODO VAI SER VIRTUAL E, QUE PODERÁ SER SOBRESCRITO EM
+    //UMA CLASSE DERIVADA.
+    class Base //NOVA CLASSE "Base" CRIADA.
+    {
+        public Base() //CONSTRUTOR
+        {
+            Console.WriteLine("Construtor da classe Base");
+        }
+        virtual public void info() //METODO "virtual" DA CLASSE "Base".
+        {
+            //NÃO FAZ SENTIDO UMA IMPLEMENTAÇÃO NO BLOCO DE "Base" QUE É "virtual" POIS, ELE SERÁ SOBREESCRITO.
+        }
+    }
+    class Derivada1 : Base //NOVA CLASSE "Derivada1" CRIADA QUE HERDA AS PROPRIEDADES DA CLASSE "Base".
+    {
+        public Derivada1() //CONSTRUTOR
+        {
+            Console.WriteLine("Construtor da classe Derivada1");
+        }
+
+        override public void info() //METODO "override" DA CLASSE "Derivada 1" QUE UTILIZA-SE DO MESMO METODO "info()" PRESENTE NA CLASSE "Base" 
+                                    //SOBRESCREVENDO O "info()" DA CLASSE BASE. QUANDO O METODO "info()" FOR CHAMADO NOVAMENTE, ELE IRÁ IMPRIMIR ESTE
+                                    //PRINT ("Derivada1") E NÃO MAIS O "info()" DA CLASSE "Base".
+        {
+            Console.WriteLine("Derivada1");
+        }
+    }
+    class Derivada2 : Derivada1 //NOVA CLASSE "Derivada2" CRIADA QUE HERDA AS PROPRIEDADES DA CLASSE "Derivada1".
+    {
+        public Derivada2() //CONSTRUTOR
+        {
+            Console.WriteLine("Construtor da classe Derivada2");
+        }
+
+        override public void info() //METODO "override" DA CLASSE "Derivada 2" QUE UTILIZA-SE DO MESMO METODO "info()" PRESENTE NA CLASSE "Derivada1" 
+                                    //SOBRESCREVENDO O "info()" DESTA CLASSE. QUANDO O METODO "info()" FOR CHAMADO NOVAMENTE, ELE IRÁ IMPRIMIR ESTE
+                                    //PRINT ("Derivada2") E NÃO MAIS O "info()" DA CLASSE "Derivada1".
+        {
+            Console.WriteLine("Derivada2");
+        }
+
+    class Program//CLASSE PADRÃO JÁ EXISTENTE.
+        {
+            static void Main()//METODO PADRÃO JÁ EXISTENTE. 
+            {
+                //POR ISTANCIAÇÃO 
+                Derivada1 derivada1 = new Derivada1();//ISTANCIAÇÃO FEITA PARA QUE SE PODESSE CHAMAR AMBAS AS "Derivadas" "override"
+                Derivada2 derivada2 = new Derivada2();//ISTANCIAÇÃO FEITA PARA QUE SE PODESSE CHAMAR AMBAS AS "Derivadas" "override"
+
+                derivada1.info(); //"info()" QUE IMPRIME A "derivada1" 
+                derivada2.info(); //"info()" QUE IMPRIME A "derivada2"
+
+
+                //POR REFERENCIA:
+
+                Base Ref; //REFERENCIA FEITA PARA QUE SE POSSA CHAMAR OS "info()" APONTADOS NA "Ref".
+
+                Ref = derivada1; //"Ref" QUE FAZ A REFERENCIA A "derivada1"
+
+                Ref.info(); //"info()" QUE IMPRIME A REFERENCIA A "derivada1"
+
+                //LEMBRANDO QUE, A REFERENCIA QUE FOR CHAMADA EM "Ref" (derivada1, derivada2), SERÁ IMPRESSA EM "Ref.info()".
+            }
+        }
+    }
+}
