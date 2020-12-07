29e7c30627ee804911b913d09ebb298e8f377c4b
Mario Torres <71908935+MarioTorres09032018@users.noreply.github.com>
2020-12-06 22:46:59 -0300

    MEUS ESTUDOS 2020

---
+++ Console_INDEXADORES_DE_CLASSES/Console_INDEXADORES_DE_CLASSES/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Console_INDEXADORES_DE_CLASSES
+    //INDEXADOR É UM MEMBRO DE UMA CLASSE QUE PERMITE AOS OBJETOS DA CLASSE SEREM INDEXADOS COMO SE FOSSEM ARRAYS. 
+{
+    class Carro //CLASSE CRIADA "carro"
+    {
+        private int[] velMax = new int[5] { 80, 120, 160, 240, 300 };//PROPRIEDADE PRIVADA ARRAY CRIADA COM 5 POSIÇÕES.
+
+        //UTILIZANDO ESTA CHAMADA PELA PROPRIEDADE "velMax" ATRAVÉS DOS ACESSORS "get And set" COM INDEXADOR:
+
+        public int this [int i] //CHAMAMOS UMA PROPRIEDADE COM  NOME "Vm", LEMBRANDO QUE COM PARENTESES() SE TORNARIA UM METODO, SEM OS
+                      //PARENTESES() SE TORNA UMA PROPRIEDADE, QUE RECEBERÁ UM ACESSADOR "get And set".
+
+        {
+            get //SE UTILIZARMOS SOMENTE O "get" SERÁ UMA PROPRIEDADE SOMENTE "LEITURA" (OBTENDO O VALOR DA PROPRIEDADE).
+                //(READY ONLY)
+            {
+                return velMax[i]; //UTILIZAMOS O "return" PARA DIZER QUE A PROPRIEDADE "Vm" RECEBE O VALOR DE "velMax".
+            }
+            set //SE UTILIZARMOS SOMENTE O "set" SERÁ UMA PROPRIEDADE DE "ESCRITA"(ALTERANDO O VALOR DA PROPRIEDADE).
+                //(WRITE ONLY)
+
+            //"get And set" SERÁ DE LEITURA E ESCRITA (READY AND WRITE).
+            {
+                if (value < 0)//O VALOR ATRIBUIDO DA PROPRIEDADE "velMax" EM "set" FICA ARMAZENADO DENTRO DE "value", QUE
+                              //PODE SER UTILIZADO A QUALQUER MOMENTO. PORTANTO AQUI REALIZAMOS UM TESTE PARA PERGUNTAR SE O
+                              //VALOR PASSADO "value" FOR MENOR (<) DO QUE ZERO "0".(VELOCIDADE NÃO EXISTE VALOR MENOR QUE 0)
+                              //ENTÃO:
+                {
+                    velMax[i] = 0;//PASSA-SE UM VALOR MÍNIMO ACEITÁVEL DO ARRAY NA POSIÇÃO[i] QUE É "0".
+                }
+                else if (value > 300) //LIMITAMOS TAMBÉM A VELOCIDADE MÁXIMA QUE O CARRO PODE TER QUE É "300".
+                {
+                    velMax[i] = 300;
+                }
+                else
+                {
+                    velMax[i] = value; //ESTA CONCLUSÃO É SE O VALOR ESTÁ NA FAIXA DE VELOCIDADE ENTRE "0 E 300"."velMax" RECEBE
+                                       //"value".
+                }
+            }
+        }
+
+        //FEITO O PROCESSO "get/set" AGORA PODEMOS USAR A PROPRIEDADE PRIVADA "int[] velMax" INCLUSIVE DENTRO DA PRÓPRIA CLASSE
+        //"Carro" BASTA CHAMAR O VALOR OBTIDO EM "get/set" DO ARRAY DE 5 POSIÇÕES.
+
+    }
+    class Program //CLASSE PADRÃO JÁ EXISTENTE "Program".
+    {
+        static void Main()//METODO PADRÃO JÁ EXISTENTE.
+        {
+            Carro c1 = new Carro(); //INSTANCIANDO UM NOVO CARRO
+
+            c1[4] = 299; //ATRIBUINDO UM NOVO VALOR DE VELOCIDADE AO VETOR "C1" NA POSIÇÃO[4] (ATRAVÉS DO ACESSOR "set").
+                         //LEMBRANDO QUE O VALOR MINIMO É "0" E MAXIMO É 300. SE EU DER UM "SET" DE 301 POR EXEMPLO NÃO SETARÁ O NOVO VALOR. 
+
+            Console.WriteLine("velocidade {0}", c1[4]);//OBTIDO O VALOR DA VELOCIDADE PARA "c1", DO ARRAY NA POSIÇÃO[4]X
+                                                       //(UTILIZANDO O ACESSOR "get").
+        }
+    }
+}
---
+++ Console_INTERFACES/Console_INTERFACES/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Console_INTERFACES
+
+//PARECIDO COM AS CLASSES ABSTRATAS, PORÉM AS INTERFACES SÓ IMPLEMENTAM METODOS, O PROTOTIPO DOS METODOS. TODA CLASSE QUE IMPLEMENTAR UMA INTERFACE,
+//OBRIGATORIAMENTE DEVE IMPLEMENTAR SEUS METODOS. NÃO CONTÉM CAMPOS E, UMA CLASSE PODE HERDAR MAIS DE UMA INTERFACE. PODEMOS IMPLEMENTAR QUANTAS 
+//INTERFACES FOREM NECESSÁRIAS PARA COMPOSIÇÃO DA MINHA CLASSE. AS DIFERENÇAS SÃO QUE 'INTERFACES' NÃO PODEM TER NEM 'CONSTRUTOR', NEM 'DESTRUTOR'.  
+//NÃO PODEMOS DEFINIR METODOS 'OPERATOR', NÃO PODEMOS DEFINIR MEMBROS 'STATIC'.
+{
+
+    public interface Veiculo //INTERFACE VEICULO CRIADA
+    {
+        void ligar(); //AQUI IMPLEMENTAMOS O RETORNO "void" DO METODO E A ASSINATURA "ligar" DE PARAMETROS DE ENTRADA() VAZIO.  
+        void desligar(); //AQUI IMPLEMENTAMOS O RETORNO "void" DO METODO E A ASSINATURA "desligar" DE PARAMETROS DE ENTRADA() VAZIO.
+        void info(); //AQUI OBRIGAMOS E IMPLEMENTAMOS O RETORNO "void" DO METODO E A ASSINATURA "info" DE PARAMETROS DE ENTRADA() VAZIO.
+    }
+
+    public interface Combate //INTERFACE COMBATE CRIADA
+    {
+        void disparar(); //AQUI IMPLEMENTAMOS O RETORNO "void" DO METODO E A ASSINATURA "disparar" DE PARAMETROS DE ENTRADA() VAZIO.
+    }
+
+    class Carro:Veiculo,Combate //CLASSE "Carro" CRIADA COM IMPLEMENTAÇÃO DAS INTERFACES HERDADAS "Veiculo e Combate".
+    {
+        public bool ligado;
+
+        private int municao;
+
+        public Carro() //CONSTRUTOR
+        {
+            setMunicao (100);
+        }
+
+        public void setMunicao(int qtde)
+        {
+            this.municao = qtde;
+        }
+        public void ligar()
+        {
+            this.ligado = true;
+        }
+        public void desligar()
+        {
+            this.ligado = false;
+        }
+        public void disparar() 
+        {
+
+        }
+        public void info()
+        {
+            //PROGRAMA NÃO FAZ NADA. APENAS PARA ENTENDIMENTO DO CONCEITO DE "INTERFACES"
+        }
+    }
+
+    class Program //CLASSE PADRÃO JÁ EXISTENTE
+    {
+        static void Main() //METODO PADRÃO JÁ EXISTENTE
+        {
+            Carro c1 = new Carro();
+        }
+    }
+}
